41903cceac7ac20f92668267d1a0dc7690b1050e
hidecard <hidecard1500@gmail.com>
2025-06-23 14:12:43 +0630

    day1

--- resources/js/Pages/Auth/Signup.vue
+++ resources/js/Pages/Auth/Signup.vue
@@ -1,5 +1,28 @@
 <script setup>
-// UI-only version, no logic or dynamic bindings
+import Master from '../Master.vue'
+defineOptions({
+    layout: Master,
+})
+import { toast } from 'vue3-toastify';
+import { useForm } from '@inertiajs/vue3';
+const form = useForm({
+    firstName: null,
+    lastName: null,
+    email: null,
+    phone: null,
+    password: null,
+});
+const submit = () => {
+    form.post(route('user.register.verify'), {
+        onSuccess: () => {
+            sessionStorage.setItem('source', 'user.register.post');
+            toast.success('Verify your email');
+        },
+        onError: () => {
+            toast.error(form.errors.message);
+        },
+    });
+}
 </script>
 
 <template>
@@ -7,54 +30,55 @@
         <title>FreshCart - SignUp</title>
     </Head>
 
-    <div class="border-bottom shadow-sm">
-        <nav class="navbar navbar-light py-2">
+    <div class="shadow-sm border-bottom">
+        <nav class="py-2 navbar navbar-light">
             <div class="container justify-content-center justify-content-lg-between">
-                <a class="navbar-brand" href="#">
-                    <img src="/public/images/yha_logo.png" alt="" class="d-inline-block align-text-top w-25" />
-                </a>
+                <Link class="navbar-brand" :href="route('index')">
+                    <img src="/public/images/yha_logo.png" alt="" class="align-text-top d-inline-block w-25" />
+                </Link>
                 <span class="navbar-text">
                     Already have an account?
-                    <a href="#" class="text-main" style="color: #ff6c0f;">Sign in</a>
+                    <Link :href="route('user.login')" class="text-main" style="color: #ff6c0f;">Sign in</Link>
                 </span>
             </div>
         </nav>
     </div>
 
     <!-- Section -->
-    <section class="my-lg-14 my-8">
+    <section class="my-8 my-lg-14">
         <!-- Container -->
         <div class="container">
             <!-- Row -->
             <div class="row justify-content-center align-items-center">
-                <div class="col-12 col-md-6 col-lg-4 order-lg-1 order-2">
+                <div class="order-2 col-12 col-md-6 col-lg-4 order-lg-1">
                     <!-- Image -->
                     <img src="/public/images/yha_logo.png" alt="" class="img-fluid" />
                 </div>
                 <!-- Form Column -->
-                <div class="col-12 col-md-6 offset-lg-1 col-lg-4 order-lg-2 order-1">
-                    <div class="mb-lg-9 mb-5">
+                <div class="order-1 col-12 col-md-6 offset-lg-1 col-lg-4 order-lg-2">
+                    <div class="mb-5 mb-lg-9">
                         <h1 class="mb-1 h2 fw-bold">Get Start Shopping</h1>
                         <p>Welcome to FreshCart! Enter your email to get started.</p>
                     </div>
                     <!-- Form -->
-                    <form class="row g-3">
+                    <form @submit.prevent="submit" class="row g-3">
                         <div class="row g-3">
                             <!-- First Name -->
                             <div class="col">
-                                <input type="text" class="form-control" placeholder="First name" id="fname" required />
+                                <input v-model="form.firstName" type="text" class="form-control" placeholder="First name" id="fname" required />
                             </div>
                             <!-- Last Name -->
                             <div class="col">
-                                <input type="text" class="form-control" placeholder="Last name" id="lname" required />
+                                <input v-model="form.lastName" type="text" class="form-control" placeholder="Last name" id="lname" required />
                             </div>
                             <!-- Email -->
                             <div class="col-12">
-                                <input type="email" class="form-control" id="email" placeholder="Email" required />
+                                <input v-model="form.email" type="email" class="form-control" id="email" placeholder="Email" required />
                             </div>
                             <!-- Phone -->
                             <div class="col-12">
                                 <input
+                                    v-model="form.phone"
                                     type="text"
                                     class="form-control"
                                     id="phone"
@@ -66,6 +90,7 @@
                             <!-- Password -->
                             <div class="col-12">
                                 <input
+                                    v-model="form.password"
                                     type="password"
                                     class="form-control"
                                     id="password"
