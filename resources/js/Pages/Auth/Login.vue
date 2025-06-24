a905ae2fb6bd431f8ffaeaef53bf75d57bfd418f
hidecard <hidecard1500@gmail.com>
2025-06-24 13:55:33 +0630

    Category

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,71 +1,100 @@
 <script setup>
-// UI-only code, no logic or dynamic bindings
-</script>
+import Master from '../Master.vue'
+defineOptions({
+    layout: Master,
+})
 
+import { toast } from 'vue3-toastify';
+import { useForm } from '@inertiajs/vue3';
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+})
+
+const submit = () => {
+    form.post(route('user.login.post'), {
+        onSuccess: () => {
+            toast.success("Login successful!");
+        },
+        onError: () => {
+            toast.error(form.errors.message);
+        },
+    });
+}
+</script>
 <template>
+
     <Head>
         <title>FreshCart - Login</title>
     </Head>
     <!-- navigation -->
-    <div class="border-bottom shadow-sm">
-        <nav class="navbar navbar-light py-2">
+    <div class="shadow-sm border-bottom">
+        <nav class="py-2 navbar navbar-light">
             <div class="container justify-content-center justify-content-lg-between">
-                <a class="navbar-brand" href="/">
-                    <img src="/public/images/yha_logo.png" alt="FreshCart Logo" class="d-inline-block align-text-top w-25">
-                </a>
+                <Link class="navbar-brand" :href="route('index')">
+                <img :src="'images/freshcart-logo.svg'" alt="" class="align-text-top d-inline-block">
+                </Link>
                 <span class="navbar-text">
                     Don't have an account?
-                    <a href="/register" class="text-primary link-success">Sign Up</a>
+                    <Link :href="route('user.register')" class="text-primary link-success">Sign Up
+                    </Link>
+
                 </span>
             </div>
         </nav>
     </div>
 
     <!-- section -->
-    <section class="my-lg-14 my-8">
+    <section class="my-8 my-lg-14">
         <div class="container">
             <!-- row -->
             <div class="row justify-content-center align-items-center">
-                <div class="col-12 col-md-6 col-lg-4 order-lg-1 order-2">
+                <div class="order-2 col-12 col-md-6 col-lg-4 order-lg-1">
                     <!-- img -->
-                    <img src="/public/images/signin-g.svg" alt="Sign In" class="img-fluid">
+                    <img :src="'images/signin-g.svg'" alt="" class="img-fluid">
                 </div>
                 <!-- col -->
-                <div class="col-12 col-md-6 offset-lg-1 col-lg-4 order-lg-2 order-1">
-                    <div class="mb-lg-9 mb-5">
+                <div class="order-1 col-12 col-md-6 offset-lg-1 col-lg-4 order-lg-2">
+                    <div class="mb-5 mb-lg-9">
                         <h1 class="mb-1 h2 fw-bold">Sign in to FreshCart</h1>
                         <p>Welcome back to FreshCart! Enter your email to get started.</p>
                     </div>
 
                     <div>
-                        <form class="row g-3">
+                        <form @submit.prevent="submit" class="row g-3">
                             <!-- row -->
+
                             <div class="col-12">
                                 <!-- input -->
-                                <input type="email" class="form-control" id="email" placeholder="Email" required>
-                                <small class="text-danger"></small>
+                                <input type="email" class="form-control" id="email" placeholder="Email"
+                                    v-model="form.email" required>
+                                <small class=" text-danger">{{ form.errors.name }}</small>
                             </div>
                             <div class="col-12">
                                 <!-- input -->
-                                <input type="password" class="form-control" id="password" placeholder="Password" required>
-                                <small class="text-danger"></small>
+                                <input type="password" class="form-control" id="password" placeholder="Password"
+                                    v-model="form.password" required>
+                                <small class=" text-danger">{{ form.errors.password }}</small>
+
                             </div>
                             <div class="d-flex justify-content-between">
                                 <!-- form check -->
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="flexCheckDefault">
-                                    <!-- label -->
-                                    <label class="form-check-label" for="flexCheckDefault">Remember me</label>
+                                    <input class="form-check-input" type="checkbox" v-model="form.remember"
+                                        id="flexCheckDefault">
+                                    <!-- label --> <label class="form-check-label" for="flexCheckDefault">
+                                        Remember me
+                                    </label>
                                 </div>
                             </div>
                             <!-- btn -->
-                            <div class="col-12 d-grid">
-                                <button type="button" class="btn main-theme">Sign In</button>
+                            <div class="col-12 d-grid"> <button type="submit" class="btn main-theme">Sign In</button>
                             </div>
                             <!-- link -->
-                            <div>
-                                Forgot your password?
-                                <a href="/reset-password" class="text-primary link-success">Reset Password</a>
+                            <div>Forgot your password?
+                                <Link :href="route('user.resetpwd')" class="text-primary link-success">Reset Password
+                                </Link>
                             </div>
                         </form>
                     </div>
